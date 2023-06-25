908f9189b85d594d1cae57bc1b94d90c6aff226d
Pritchad25 <pricts29@gmail.com>
2023-06-25 14:03:27 -0700

    removal of unused parameter that was causing compilation errors.

--- monty.h
+++ monty.h
@@ -70,7 +70,7 @@ void f_mul(stack_t **head, unsigned int counter);
 void f_mod(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
 void f_pstr(stack_t **head, unsigned int counter);
-void f_rotl(stack_t **head, unsigned int counter);
+void f_rotl(stack_t **head);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void addnode(stack_t **head, int n);
 void addqueue(stack_t **head, int n);
